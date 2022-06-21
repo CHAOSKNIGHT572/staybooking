82b4cf48b8f14d5f994cee72aa6427e1ea0781d5
CHAOSKNIGHT572 <bluewhale1991@hotmail.com>
2022-06-21 00:51:09 -0700

    Add password encoder to Register service
    Add password encoder in register service.

--- src/main/java/com/example/staybooking/service/RegisterService.java
+++ src/main/java/com/example/staybooking/service/RegisterService.java
@@ -7,6 +7,7 @@ import com.example.staybooking.model.UserRole;
 import com.example.staybooking.repository.AuthorityRepository;
 import com.example.staybooking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +20,14 @@ public class RegisterService {
 
     private AuthorityRepository authorityRepository;
 
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
-    public RegisterService(UserRepository userRepository, AuthorityRepository authorityRepository) {
+    public RegisterService(UserRepository userRepository, AuthorityRepository authorityRepository,
+                           PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     //Transactional annotation to guarantee atomic operation in DB.
@@ -31,6 +36,8 @@ public class RegisterService {
         if (userRepository.existsById(user.getUsername())) {
             throw new UserAlreadyExistException("User Already Exists!!!");
         }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setEnabled(true);
         userRepository.save(user);
         authorityRepository.save(new Authority(user.getUsername(), userRole.name()));
     }
